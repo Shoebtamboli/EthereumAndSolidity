33dcf47d144843920291494f3ba7ca2b475e92ae
Shoeb Tamboli <shoeb.tamboli@tymlez.com>
2020-05-26 00:42:22 +0200

    lottery contract frontEnd

---
+++ Lottery react/src/lottery.js
@@ -0,0 +1,8 @@
+import web3 from './web3';
+
+const address = '0x64C6cC474C466B690AD42C1055e4c8d2d93ED5fc';
+
+const abi =
+[{ "constant": true, "inputs": [], "name": "manager", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "pickWinner", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getPlayers", "outputs": [{ "name": "", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "enter", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "players", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }];
+
+export default new web3.eth.Contract(abi, address);
